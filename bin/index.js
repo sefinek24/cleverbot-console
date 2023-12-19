3440139d73d6263c8a3dc2b46b97fffaf247557a
Sefinek <contact@sefinek.net>
2023-12-19 04:15:45 +0100

    Some quality fixes

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 
 const { magenta, green, red } = require('kleur');
-console.log(magenta('Starting a new session...'));
-
 const CleverBot = require('free-cleverbot');
 const readline = require('readline');
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-
 const context = [];
 
-async function askCleverbot(message) {
+console.log(magenta('Starting a new session...'));
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+const askCleverbot = async message => {
 	try {
 		const response = await CleverBot(message, context);
 		context.push(message);
 		context.push(response);
+
 		console.log(green(response));
 	} catch (err) {
 		console.error(red(`Sorry, there was an error communicating with the Cleverbot API.\n\n${err.stack}`));
 	}
 	rl.prompt();
-}
+};
 
 rl.on('line', async line => {
 	await askCleverbot(line);
